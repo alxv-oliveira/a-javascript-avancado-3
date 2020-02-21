ed49c8418907b0a29e9ff398d667104196ce4e9c
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-21 16:22:54 -0300

    07.Trabalhando com módulos do ES2015!

--- client/js/app-es6/controllers/NegociacaoController.js
+++ client/js/app-es6/controllers/NegociacaoController.js
@@ -1,3 +1,12 @@
+import {ListaNegociacoes} from '../models/ListaNegociacoes';
+import {Mensagem} from '../models/Mensagem';
+import {Negociacao} from '../models/Negociacao';
+import {NegociacoesView} from '../views/NegociacoesView';
+import {MensagemView} from '../views/MensagemView';
+import {NegociacaoService} from '../services/NegociacaoService';
+import {DateHelper} from '../helpers/DateHelper';
+import {Bind} from '../helpers/Bind';
+
 class NegociacaoController {
 
     constructor() {
@@ -91,4 +100,9 @@ class NegociacaoController {
 
         this._inputData.focus();
     }
+}
+
+let negociacaoController = new NegociacaoController();
+export function currentInstance() {
+    return negociacaoController;
 }
